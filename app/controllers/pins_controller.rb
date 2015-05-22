dbdc57b918bd0b6e8ceabcdb89deda8fb7fa6ecc
Raquel <raquel_cuellar@es-cat.org>
2015-05-21 21:53:25 -0700

    -Pin_board

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,6 +1,6 @@
 class PinsController < ApplicationController
-	before_action :find_pin, only: [:show, :edit, :update, :destroy, :upvote]
-	before_action :authenticate_user!, except: [ :index, :show ]
+	before_action :find_pin, only: [:show, :edit, :update, :destroy, :upvote, :downvote]
+	before_action :authenticate_user!, except: [:index, :show]
 
 	def index
 		@pins = Pin.all.order("created_at DESC")
@@ -14,13 +14,13 @@ class PinsController < ApplicationController
 	end
 
 	def create
-		@pin = current_user.pins.build(pin_params)
-
-		if @pin.save
-			redirect_to @pin, notice: "Sucessfully created new Pin"
-		else
-			render 'new'
-		end
+    	@pin = current_user.pins.build(pin_params)
+    	
+    	if @pin.save
+    		redirect_to @pin, notice: "Pin was successfully created"
+    	else
+    		render 'new'
+    	end
 	end
 
 	def edit
--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,6 +2,9 @@ class Pin < ActiveRecord::Base
 	acts_as_votable
 	belongs_to :user
 
-	has_attached_file :image, styles: { medium: "300x300>" }
+	has_attached_file :image, :styles => { :medium => "300x300>" }
 	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+	validates :image, presence: true
+	validates :title, presence: true 
+	validates :description,  presence: true
 end
